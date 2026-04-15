f25e9cda2913e7f49680bd5b3cd7aba514150203
Dan Brumleve <jdb1729@gmail.com>
2026-04-15 14:45:20 -0500

    aptata

--- proba_corpus.sh
+++ proba_corpus.sh
@@ -17,9 +17,9 @@ par=0
 comp_cc_ok=0
 comp_ccc_ok=0
 comp_cc_ldi_ok=0
-comp_ccc_cc_ok=0
+comp_ccc_ld_ok=0
 par_cc_ldi=0
-par_ccc_cc=0
+par_ccc_ld=0
 
 # curre cum limite temporis per backgrounding
 curre_cum_limite() {
@@ -36,9 +36,9 @@ curre_cum_limite() {
 
 printf "%-16s   -------- compilātiō ---------    ----------- cursus ----------\n" ""
 printf "%-16s" "PLICA"
-printf " %7s" "CC/CC" "CCC/LDI" "CC/LDI" "CCC/CC"
+printf " %7s" "CC/LD" "CCC/LDI" "CC/LDI" "CCC/LD"
 printf " "
-printf " %7s" "CC/CC" "CCC/LDI" "CC/LDI" "CCC/CC"
+printf " %7s" "CC/LD" "CCC/LDI" "CC/LDI" "CCC/LD"
 printf "\n"
 
 for fons in "$DIR"/*.c; do
@@ -49,18 +49,18 @@ for fons in "$DIR"/*.c; do
     bin_cc="$TMP/${nomen}_cc"
     bin_ccc="$TMP/${nomen}_ccc"
     bin_cc_ldi="$TMP/${nomen}_cc_ldi"
-    bin_ccc_cc="$TMP/${nomen}_ccc_cc"
+    bin_ccc_ld="$TMP/${nomen}_ccc_ld"
     out_cc="$TMP/${nomen}_cc.out"
     out_ccc="$TMP/${nomen}_ccc.out"
     out_cc_ldi="$TMP/${nomen}_cc_ldi.out"
-    out_ccc_cc="$TMP/${nomen}_ccc_cc.out"
+    out_ccc_ld="$TMP/${nomen}_ccc_ld.out"
 
     # 1. cc/cc
-    obj_cc_cc="$TMP/${nomen}_cc_cc.o"
-    $CC -std=c99 -O0 -c -o "$obj_cc_cc" "$fons" 2>/dev/null
+    obj_cc_ld="$TMP/${nomen}_cc_ld.o"
+    $CC -std=c99 -O0 -c -o "$obj_cc_ld" "$fons" 2>/dev/null
     cc_comp=$?
     if [ $cc_comp -eq 0 ]; then
-        $CC -o "$bin_cc" "$obj_cc_cc" 2>/dev/null
+        $CC -o "$bin_cc" "$obj_cc_ld" 2>/dev/null
         if [ $? -ne 0 ]; then cc_comp="link"; fi
     else cc_comp="comp"; fi
 
@@ -84,13 +84,13 @@ for fons in "$DIR"/*.c; do
 
     # 4. ccc/cc
     $CCC -Scapita -c -o "$obj_ccc" "$fons" >/dev/null 2>&1
-    ccc_cc_comp=$?
-    if [ $ccc_cc_comp -eq 0 ]; then
-        $CC -o "$bin_ccc_cc" "$obj_ccc" 2>/dev/null
-        if [ $? -ne 0 ]; then ccc_cc_comp="link"; fi
-    else ccc_cc_comp="comp"; fi
+    ccc_ld_comp=$?
+    if [ $ccc_ld_comp -eq 0 ]; then
+        $CC -o "$bin_ccc_ld" "$obj_ccc" 2>/dev/null
+        if [ $? -ne 0 ]; then ccc_ld_comp="link"; fi
+    else ccc_ld_comp="comp"; fi
 
-    cc_run="-"; ccc_run="-"; cc_ldi_run="-"; ccc_cc_run="-"
+    cc_run="-"; ccc_run="-"; cc_ldi_run="-"; ccc_ld_run="-"
     cmp1="-"; cmp2="-"; cmp3="-"
 
     # curre cc/cc
@@ -125,24 +125,24 @@ for fons in "$DIR"/*.c; do
     fi
 
     # curre ccc/cc et compara cum cc/cc
-    if [ "$ccc_cc_comp" = "0" ]; then
-        comp_ccc_cc_ok=$((comp_ccc_cc_ok + 1))
-        curre_cum_limite "$bin_ccc_cc" > "$out_ccc_cc" 2>&1
-        ccc_cc_run=$?
-        if [ "$cc_run" != "-" ] && [ "$ccc_cc_run" -eq 0 ] && [ "$cc_run" -eq 0 ]; then
-            if diff -q "$out_cc" "$out_ccc_cc" >/dev/null 2>&1; then
-                ccc_cc_run="par"; par_ccc_cc=$((par_ccc_cc + 1))
-            else ccc_cc_run="dis"; fi
+    if [ "$ccc_ld_comp" = "0" ]; then
+        comp_ccc_ld_ok=$((comp_ccc_ld_ok + 1))
+        curre_cum_limite "$bin_ccc_ld" > "$out_ccc_ld" 2>&1
+        ccc_ld_run=$?
+        if [ "$cc_run" != "-" ] && [ "$ccc_ld_run" -eq 0 ] && [ "$cc_run" -eq 0 ]; then
+            if diff -q "$out_cc" "$out_ccc_ld" >/dev/null 2>&1; then
+                ccc_ld_run="par"; par_ccc_ld=$((par_ccc_ld + 1))
+            else ccc_ld_run="dis"; fi
         fi
     fi
 
     # color per cellam
     R="\033[31m"; Y="\033[33m"; G="\033[32m"; Z="\033[0m"
     has_bad=0
-    for v in "$ccc_comp" "$cc_ldi_comp" "$ccc_cc_comp"; do
+    for v in "$ccc_comp" "$cc_ldi_comp" "$ccc_ld_comp"; do
         case "$v" in comp|link) has_bad=1 ;; esac
     done
-    for v in "$ccc_run" "$cc_ldi_run" "$ccc_cc_run"; do
+    for v in "$ccc_run" "$cc_ldi_run" "$ccc_ld_run"; do
         case "$v" in dis) has_bad=1 ;; -|0|par) ;; *) has_bad=1 ;; esac
     done
     case "$cc_run" in -|0) ;; *) has_bad=1 ;; esac
@@ -153,11 +153,11 @@ for fons in "$DIR"/*.c; do
         printf "%-16s" "$nomen"
     fi
 
-    for v in "$cc_comp" "$ccc_comp" "$cc_ldi_comp" "$ccc_cc_comp"; do
+    for v in "$cc_comp" "$ccc_comp" "$cc_ldi_comp" "$ccc_ld_comp"; do
         case "$v" in comp|link) printf " ${R}%7s${Z}" "$v" ;; *) printf " %7s" "$v" ;; esac
     done
     printf " "
-    for v in "$cc_run" "$ccc_run" "$cc_ldi_run" "$ccc_cc_run"; do
+    for v in "$cc_run" "$ccc_run" "$cc_ldi_run" "$ccc_ld_run"; do
         case "$v" in
             par) printf " ${G}%7s${Z}" "$v" ;;
             dis) printf " ${R}%7s${Z}" "$v" ;;
@@ -174,7 +174,7 @@ echo "casus totales:           $tot"
 echo "cc/cc compilavit:        $comp_cc_ok / $tot"
 echo "ccc/ldi compilavit:      $comp_ccc_ok / $tot"
 echo "cc/ldi compilavit:       $comp_cc_ldi_ok / $tot"
-echo "ccc/cc compilavit:       $comp_ccc_cc_ok / $tot"
+echo "ccc/cc compilavit:       $comp_ccc_ld_ok / $tot"
 echo "par cc/cc vs ccc/ldi:    $par / $tot"
 echo "par cc/cc vs cc/ldi:     $par_cc_ldi / $tot"
-echo "par cc/cc vs ccc/cc:     $par_ccc_cc / $tot"
+echo "par cc/cc vs ccc/cc:     $par_ccc_ld / $tot"
